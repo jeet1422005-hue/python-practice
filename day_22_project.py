dc9004fea9537b4f79c4277ad36dca6060cfa2bd
Jeet Mondal <jeet1422005@gmail.com>
2026-06-18 19:44:11 +0530

    Day 22 - Drought risk analyzer project

---
+++ day_22_project.py
@@ -0,0 +1,13 @@
+import pandas as pd
+df = pd.read_csv("wb_climate_new.csv")
+
+
+def assess_risk(row):
+     if row["avg_rainfall_mm"] < 1500 and row["avg_temp_c"] > 27:
+        return "High Risk"
+     elif row["avg_rainfall_mm"] < 1500 and row["avg_temp_c"] <= 27:
+        return "Moderate Risk"
+     else:
+        return "Low Risk"
+df["drought_risk"] = df.apply(assess_risk, axis=1)   
+print(df)        
